2eb89038bf048d6df3ac351a7ffd36633c107702
Marcelo Gonçalves de Souza <marcelo.souza@dnweb.com.br>
2024-12-18 18:55:33 -0300

    TypeScript test.

--- README.md
+++ README.md
@@ -5,3 +5,15 @@
 - Em src: node .\index.js - localhost:3000
 
 ## Instalação do TypeScript
+
+- npm i typescript
+- npm uninstall typescript
+- npm i typescript --save-dev ou -D
+- npm i @types/node @types/express -D
+
+## Executando dependências
+
+- npx tsc --init
+- npm install --save-dev @tsconfig/recommended
+- npx tsc
+- node lib/index.js
---
+++ api-course/lib/index.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+// Rota básica
+app.get("/", (req, res) => {
+    res.send("Node.js");
+});
+// Criação do servidor
+app.listen(3000, () => {
+    console.log("Servidor ativo na porta 3000.");
+});
+//# sourceMappingURL=index.js.map
